fc0276811c987f9122c8ff25014cd6debbf478ac
Antoine Gannat <antoine.gannat@epitech.eu>
2020-04-14 19:50:04 +0200

    fix missing css

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
 import Snotify from 'vue-snotify'
-// Import the router
 import router from './router'
-// Import the store
 import store from './store'
 
 // Vue snotify
